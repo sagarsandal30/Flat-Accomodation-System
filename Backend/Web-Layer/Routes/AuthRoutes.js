7f530d8e553a65eaeb6b48cc81c579bec3291fc6
sagar <sagarsandal9@gmail.com>
2026-04-30 18:04:02 +0530

    made changes in the AuthRoutes.js

--- Backend/Web-Layer/Routes/AuthRoutes.js
+++ Backend/Web-Layer/Routes/AuthRoutes.js
@@ -2,9 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const {login,register} = require("../Controller/AuthController");
+const {login,register,logout} = require("../Controller/AuthController");
+const authMiddleware = require("../Middleware/AuthMiddleware");
 
 router.post("/auth/register", register);
 router.post("/auth/login", login);
+
+router.use(authMiddleware);
+router.post("/auth/logout", logout);
 
 module.exports = router;
